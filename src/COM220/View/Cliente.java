ec3e18cff254cacf285131b0ae54b68d54a3181b
natanmorais <morais_natan@hotmail.com>
2015-12-02 19:07:47 -0200

    Adicionado todos os modelos do projeto

---
+++ src/COM220/View/Cliente.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package COM220.View;
+
+import COM220.Utils.Constants;
+import java.io.Serializable;
+
+/**
+ *
+ * @author natanmorais
+ */
+public class Cliente implements Serializable{
+    String CPF, Nome, Endereço;
+    int Telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(String CPF, String Nome, String Endereço, int Telefone) {
+        this.CPF = CPF;
+        this.Nome = Nome;
+        this.Endereço = Endereço;
+        this.Telefone = Telefone;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getEndereço() {
+        return Endereço;
+    }
+
+    public void setEndereço(String Endereço) {
+        this.Endereço = Endereço;
+    }
+
+    public int getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(int Telefone) {
+        this.Telefone = Telefone;
+    }
+    
+    
+}
---
+++ src/COM220/View/Quarto.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package COM220.View;
+
+import COM220.Utils.Constants;
+import java.io.Serializable;
+
+/**
+ *
+ * @author natanmorais
+ */
+public class Quarto implements Serializable{
+    int Numero;
+    double Preço;
+    String Descrição;
+
+    public Quarto() {
+    }
+
+    public Quarto(int Numero, double Preço, String Descrição) {
+        this.Numero = Numero;
+        this.Preço = Preço;
+        this.Descrição = Descrição;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public double getPreço() {
+        return Preço;
+    }
+
+    public void setPreço(double Preço) {
+        this.Preço = Preço;
+    }
+
+    public String getDescrição() {
+        return Descrição;
+    }
+
+    public void setDescrição(String Descrição) {
+        this.Descrição = Descrição;
+    }
+    
+}
