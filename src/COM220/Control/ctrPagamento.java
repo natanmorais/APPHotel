ba6f0deb9a37201d02a2cd66e2cb43bc54bba0cc
natanmorais <morais_natan@hotmail.com>
2015-12-02 20:41:53 -0200

    Adicionado todos os controles do projeto, e todos com as funções mínimas necessárias, excluindo o Controle das Reservas que ainda não contém nada.

---
+++ src/COM220/Control/ctrPagamento.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package COM220.Control;
+
+import COM220.Model.Pagamento;
+import COM220.Model.Quarto;
+import COM220.Utils.Constants;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+/**
+ *
+ * @author natanmorais
+ */
+public class ctrPagamento {
+    //Criar atributo para ter o view do Pagamento.
+    ArrayList<Pagamento> listaPagamentos = new ArrayList<>();
+
+    public void SalvaPagamento() throws Exception{
+        try {
+
+            //Gera o arquivo para armazenar o objeto
+            FileOutputStream arquivoGrav
+                    = new FileOutputStream("pagamentos.dat");
+
+            //Classe responsavel por inserir os objetos
+            ObjectOutputStream objGravar = new ObjectOutputStream(arquivoGrav);
+
+            //Grava o objeto cliente no arquivo
+            objGravar.writeObject(listaPagamentos);
+
+            objGravar.flush();
+
+            objGravar.close();
+
+            arquivoGrav.flush();
+
+            arquivoGrav.close();
+
+        } catch (Exception e) {
+
+            throw e;
+
+        }
+    }
+
+    public void BuscaPagamentos() throws Exception{
+        try {
+
+            //Carrega o arquivo
+            FileInputStream arquivoLeitura = new FileInputStream("pagamentos.dat");
+
+            //Classe responsavel por recuperar os objetos do arquivo
+            ObjectInputStream objLeitura
+                    = new ObjectInputStream(arquivoLeitura);
+
+            listaPagamentos = (ArrayList<Pagamento>) objLeitura.readObject();
+
+            objLeitura.close();
+
+            arquivoLeitura.close();
+
+        } catch (Exception e) {
+
+            throw e;
+
+        }
+    }
+    
+    public void RegistrarPagamento(Pagamento p) throws Exception {
+        BuscaPagamentos();
+        for(Pagamento pAux: listaPagamentos){
+            if(p.getReservaEfetuada().getCodigo()==pAux.getReservaEfetuada().getCodigo()){
+                p.setValor(p.getValor()+pAux.getValor());
+                listaPagamentos.remove(pAux);
+            }
+        }
+        Calendar dataEnt = null, dataSaí = null;
+        dataEnt.setTimeInMillis(p.getReservaEfetuada().getDataEntrada());
+        dataSaí.setTimeInMillis(p.getReservaEfetuada().getDataSaída());
+        double Preço = p.getReservaEfetuada().getDesconto();
+        for(Quarto q: p.getReservaEfetuada().getQuartos()){
+            Preço += q.getPreço();
+        }
+        if(p.getValor()>Preço){
+            throw new Exception("Valor de pagamento maior que o valor da Reserva!");
+        }
+        else if(p.getValor()==Preço){
+            p.setSituação(Constants.GARANTIDO);
+        }
+        else if(p.getValor()>= Preço/(dataSaí.get(Calendar.DAY_OF_YEAR)-dataEnt.get(Calendar.DAY_OF_YEAR))){
+            p.setSituação(Constants.GARANTIDO);
+        }
+        else {
+            p.setSituação(Constants.NAO_GARANTIDO);
+        }
+        listaPagamentos.add(p);
+        SalvaPagamento();
+    }
+    
+    public int VerificarPagamento(int cod) {
+        for(Pagamento p: listaPagamentos){
+            if(p.getReservaEfetuada().getCodigo() == cod){
+                return p.getSituação();
+            }
+        }
+        return Constants.ERRO;
+    }
+    
+}
--- src/COM220/Utils/Constants.java
+++ src/COM220/Utils/Constants.java
@@ -7,8 +7,11 @@ package COM220.Utils;
 
 public class Constants {
 
+    //Pagamento
+    public static final int ERRO = -1;
     public static final int NAO_PAGO = 0;
-    public static final int GARANTIDO = 1;
-    public static final int PAGO = 2;
+    public static final int NAO_GARANTIDO = 1;
+    public static final int GARANTIDO = 2;
+    public static final int PAGO = 3;
 
 }
